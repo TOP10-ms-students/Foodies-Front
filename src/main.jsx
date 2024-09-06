533ab67c7afc3b21b8dafa8df1c4c86e5f4f0dc4
Andrii Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2024-09-06 21:49:37 +0200

    feat: add .prettierrc

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from '~/App';
+import App from "~/App";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
