2c5baa94e995b4ae7bb4a5f9e86455b20204652b
Andrii Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2024-09-08 00:05:43 +0200

    feat: edit ROUTE_PATHS

--- src/routing/components/RoutesList.jsx
+++ src/routing/components/RoutesList.jsx
@@ -4,13 +4,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "~/common/layouts";
 import { HomePage } from "~/pages";
 
-import { routePaths } from "~/routing/constants";
+import { ROUTE_PATHS } from "~/routing/constants";
 
 export const RoutesList = () => (
   <Routes>
-    <Route path={routePaths.home} element={<Layout />}>
+    <Route path={ROUTE_PATHS.home} element={<Layout />}>
       <Route index element={<HomePage />} />
     </Route>
-    <Route path="*" element={<Navigate to={routePaths.home} />} />
+    <Route path="*" element={<Navigate to={ROUTE_PATHS.home} />} />
   </Routes>
 );
