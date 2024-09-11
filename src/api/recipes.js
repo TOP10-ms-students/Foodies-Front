58feff97da4811b76a35014788e29c7a813f442e
Andrii Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2024-09-11 21:43:27 +0200

    feat: finished add recipe form

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,7 +1,3 @@
 import { api } from "./ApiService";
 
-export const getAllRecipes = () => api.get("recipes");
-
-export const getRecipeById = (id) => api.get(`recipes/${id}`);
-
 export const createRecipe = (data) => api.post("recipes", data);
