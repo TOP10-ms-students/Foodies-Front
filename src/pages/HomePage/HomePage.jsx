8a4b80acf2ebaab0715e059b3bfd95ec73244f70
DmytroZhuk <dm.zhuk@gmail.com>
2024-09-13 17:08:53 +0300

    added adaptive Hero section
    created & removed: /common/components/forms/LoginForm/SignInModal.jsx (Modal logic for main 'Add recipe' btn) code moved to /common/components/custom/Hero/Hero.jsx;
    modified: /pages/HomePage/HomePage.jsx - Hero section related code moved to /Hero/Hero.jsx;
    modified: /pages/HomePage/HomePage.styled.jsx - Hero section related code moved to /Hero/Hero.styled.jsx;

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,77 +1,58 @@
+import { Alert } from "antd";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+
 import { CategoriesList } from "~/common/components/custom/CategoriesList";
 import { Hero } from "~/common/components/custom/Hero";
 import { RecipesList } from "~/common/components/custom/RecipesList";
+import { Testimonials } from "~/common/components/custom/Testimonials";
+
 import { useCategoriesQueries } from "~/common/hooks/useCategoriesQueries";
-import { Button } from "~/common/components/ui/Button";
-import SignInModal from "~/common/components/forms/LoginForm/SignInModal";
-import hero1 from "~/common/components/img/hero1_x2.jpg";
-import hero2 from "~/common/components/img/hero2_x2.jpg";
-import {
-  HeroSection,
-  HeroContentBox,
-  HeroTitle,
-  HeroSubtitle,
-  HeroImagesBox,
-  HeroImage,
-} from "./HomePage.styled";
+
+import { CategoriesAndRecipesWrapper } from "./HomePage.styled";
 
 export const HomePage = () => {
-  const { category, setCategory, resetCategory } = useCategoriesQueries();
-  const navigate = useNavigate();
+  const { categoryName, categoryId, setCategory, resetCategory } =
+    useCategoriesQueries();
+  const [error, setError] = useState(null);
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-
-  const handleAddRecipeClick = () => {
-    if (isAuthenticated) {
-      navigate("/add-recipe");
-    } else {
-      setIsModalVisible(true);
-    }
+  const handleError = (errorMessage) => {
+    setError(errorMessage);
+    resetCategory();
   };
 
-  const handleLoginSuccess = () => {
-    setIsModalVisible(false);
-    navigate("/add-recipe");
+  const handleCloseError = () => {
+    setError(null);
   };
 
   return (
     <div>
-      <HeroSection>
-        <HeroContentBox>
-          <HeroTitle>Improve Your Culinary Talents</HeroTitle>
-          <HeroSubtitle>
-            Amazing recipes for beginners in the world of cooking, enveloping
-            you in the aromas and tastes of various cuisines.
-          </HeroSubtitle>
-          <Button type="primary" onClick={handleAddRecipeClick}>
-            Add Recipe
-          </Button>
-        </HeroContentBox>
+      <Hero />
 
-        <HeroImagesBox>
-          <HeroImage size="small" imageUrl={hero1} />
-          <HeroImage size="large" imageUrl={hero2} />
-        </HeroImagesBox>
-      </HeroSection>
-
-      <SignInModal
-        visible={isModalVisible}
-        onClose={() => setIsModalVisible(false)}
-        onSuccess={handleLoginSuccess}
-      />
-
-      {category ? (
-        <RecipesList category={category} goToCategories={resetCategory} />
-      ) : (
-        <CategoriesList setCategory={setCategory} />
+      {error && (
+        <Alert
+          message="Error"
+          description={error}
+          type="error"
+          closable
+          onClose={handleCloseError}
+          style={{ marginBottom: 16 }}
+        />
       )}
 
+      <CategoriesAndRecipesWrapper>
+        {categoryName ? (
+          <RecipesList
+            category={{ name: categoryName, id: categoryId }}
+            goToCategories={resetCategory}
+            onError={handleError}
+          />
+        ) : (
+          <CategoriesList setCategory={setCategory} />
+        )}
+      </CategoriesAndRecipesWrapper>
+
       <section>
-        <div>Info block</div>
+        <Testimonials />
       </section>
     </div>
   );
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,107 +1,13 @@
-import React from "react";
 import { styled } from "styled-components";
 
-export const HeroSection = styled.section`
-  width: 100%;
-  height: 796px;
-  flex-shrink: 0;
-  border-radius: 20px;
-  color: ${({ theme }) => theme.colors.white};
-  background-color: ${({ theme }) => theme.colors.black};
+export const CategoriesAndRecipesWrapper = styled.div`
+  padding: 64px;
 
   @media ${({ theme }) => theme.media.tablet} {
-    height: 992px;
-    border-radius: 30px;
+    padding: 100px 0;
   }
 
   @media ${({ theme }) => theme.media.desktop} {
-    height: 970px;
-    border-radius: 30px;
+    padding: 120px 0;
   }
 `;
-
-export const HeroContentBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-
-  @media ${({ theme }) => theme.media.tablet},
-    ${({ theme }) => theme.media.desktop} {
-    column-gap: 40px;
-  }
-`;
-
-export const HeroTitle = styled.h1`
-  margin: 0;
-  padding-top: 194px;
-  width: 327px;
-  font-size: 40px;
-  font-weight: 800;
-  line-height: 1;
-  letter-spacing: -0.02em;
-  text-align: center;
-  text-transform: uppercase;
-
-  @media ${({ theme }) => theme.media.tablet} {
-    width: 678px;
-    padding-top: 217px;
-    font-size: 70px;
-  }
-
-  @media ${({ theme }) => theme.media.desktop} {
-    width: 875px;
-    padding-top: 154px;
-    font-size: 90px;
-  }
-`;
-
-export const HeroSubtitle = styled.p`
-  margin: 0;
-  width: 326px;
-  font-size: 14px;
-  font-weight: 500;
-  line-height: 1.42;
-  letter-spacing: -0.02em;
-  text-align: center;
-
-  @media ${({ theme }) => theme.media.tablet},
-    ${({ theme }) => theme.media.desktop} {
-    width: 577px;
-    font-size: 16px;
-    line-height: 1.5;
-  }
-`;
-
-export const HeroImagesBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin: 30px 0 0;
-`;
-
-const StyledHeroImage = styled.div`
-  width: ${({ size }) => (size === "small" ? "77px" : "190px")};
-  height: ${({ size }) => (size === "small" ? "70px" : "172px")};
-  border-radius: ${({ size }) => (size === "small" ? "15px" : "30px")};
-  flex-shrink: 0;
-  overflow: hidden;
-  background-size: cover;
-  background-position: center;
-  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
-  background-repeat: no-repeat;
-
-  @media ${({ theme }) => theme.media.tablet},
-    ${({ theme }) => theme.media.desktop} {
-    width: ${({ size }) => (size === "small" ? "128px" : "302px")};
-    height: ${({ size }) => (size === "small" ? "116px" : "273px")};
-  }
-`;
-
-export const HeroImage = ({ size, imageUrl }) => (
-  <StyledHeroImage
-    size={size}
-    style={{ backgroundImage: `url(${imageUrl})` }}
-  />
-);
