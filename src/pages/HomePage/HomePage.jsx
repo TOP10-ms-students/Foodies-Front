943ec0b24b8efc8c00e8042febd581b0d24da441
Andrii Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2024-09-13 13:49:09 +0200

    remove unused imports

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,20 +3,9 @@ import React from "react";
 import { CategoriesList } from "~/common/components/custom/CategoriesList";
 import { Hero } from "~/common/components/custom/Hero";
 import { RecipesList } from "~/common/components/custom/RecipesList";
+import { Testimonials } from "~/common/components/custom/Testimonials";
 
 import { useCategoriesQueries } from "~/common/hooks/useCategoriesQueries";
-import { Button } from "~/common/components/ui/Button";
-import hero1 from "~/common/components/img/hero1_x2.jpg";
-import hero2 from "~/common/components/img/hero2_x2.jpg";
-import {
-  HeroSection,
-  HeroContentBox,
-  HeroTitle,
-  HeroSubtitle,
-  HeroImagesBox,
-  HeroImage,
-} from "./HomePage.styled";
-import { Testimonials } from "~/common/components/custom/Testimonials";
 
 export const HomePage = () => {
   const { category, setCategory, resetCategory } = useCategoriesQueries();
